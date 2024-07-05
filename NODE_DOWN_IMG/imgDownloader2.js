10280f3204035bbebbd4707baf0208504c7e8f74
Hailex <kshitij.singh.bisht.174sis@gmail.com>
2024-07-05 21:29:21 +0530

    Favicon

--- NODE_DOWN_IMG/imgDownloader2.js
+++
@@ -1,6 +0,0 @@
-for(let i=31276 ; i<=31280 ; i++){
-    const downloadImage = require('./imgDownloader').default;
-    const imgUrl = `https://erp.psit.ac.in/assets/img/Simages/${i}.jpg`;
-    const destination = `./images/${i}.jpg`;
-    downloadImage(imgUrl, destination)
-}
